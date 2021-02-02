cf0592598717ea66d09760cc6594325ee3896c1e
Sophia <dollgirl4o7@gmail.com>
2021-02-01 22:23:35 -0800

    successfully can log in with google and log out

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
+
 import App from './component/App';
 import './index.css';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}><App /></Provider>,
   document.getElementById('root')
 );
 
